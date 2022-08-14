94538c1f893b1e7b20e948de5658666abcaf426a
SyouShiro Xu <42944065+SyouShiro@users.noreply.github.com>
2022-08-14 12:56:52 -0400

    fixed Github OAuth request Bug

--- src/main/java/com/syoushiro/tofubbs/networker/GithubNetworker.java
+++ src/main/java/com/syoushiro/tofubbs/networker/GithubNetworker.java
@@ -34,7 +34,8 @@ public class GithubNetworker {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 //.url("https://api.github.com/user?access_token=" + accessToken)
-                .url("Authorization: token " + accessToken + " https://api.github.com/user?access_token=")
+                .header("Authorization", accessToken)
+                .url(" https://api.github.com/user")
                 .build();
         try {
             Response response = client.newCall(request).execute();
