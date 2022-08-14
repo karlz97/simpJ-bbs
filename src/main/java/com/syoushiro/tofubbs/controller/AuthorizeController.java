41ea73d2fb17bf7b9a2b99bd036686acac01972a
Spicy Fried Clams <48748912+karlz97@users.noreply.github.com>
2022-08-14 12:47:50 -0400

    authorization through github
    github changed API call rule, need to fix it. see:
    https://api.github.com/user?access_token=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

--- src/main/java/com/syoushiro/tofubbs/controller/AuthorizeController.java
+++ src/main/java/com/syoushiro/tofubbs/controller/AuthorizeController.java
@@ -1,14 +1,31 @@
 package com.syoushiro.tofubbs.controller;
 
+import com.syoushiro.tofubbs.dto.AccessTokenDTO;
+import com.syoushiro.tofubbs.dto.GithubUserInforms;
+import com.syoushiro.tofubbs.networker.GithubNetworker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
-    @GetMapping("/callback")
+    @Autowired
+    private GithubNetworker githubNetworker;
+
+    @GetMapping("/callback")  //http://localhost:8080/callback
     public String callback(@RequestParam(name="code") String code,
                             @RequestParam(name="state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_url("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id("a4ad1d7966a233ad712c");
+        accessTokenDTO.setClient_secret("b72c3229c50bc94b1e925635e225c9131da6dd63");
+        String accessToken = githubNetworker.getAccessToken(accessTokenDTO);
+        System.out.println(accessToken);
+        GithubUserInforms userInforms = githubNetworker.getUserInforms(accessToken);
+        System.out.println(userInforms.getName());
         return "index";
     }
 }
---
+++ src/main/java/com/syoushiro/tofubbs/networker/GithubNetworker.java
@@ -0,0 +1,49 @@
+package com.syoushiro.tofubbs.networker;
+
+import com.alibaba.fastjson2.JSON;
+import com.syoushiro.tofubbs.dto.AccessTokenDTO;
+import com.syoushiro.tofubbs.dto.GithubUserInforms;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GithubNetworker {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(JSON.toJSONString(accessTokenDTO), mediaType);
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String tokenInform = response.body().string();
+            /*[DEBUG] System.out.println(tokenInform);*/
+            String token = tokenInform.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUserInforms getUserInforms(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                //.url("https://api.github.com/user?access_token=" + accessToken)
+                .url("Authorization: token " + accessToken + " https://api.github.com/user?access_token=")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUserInforms githubUserInforms = JSON.parseObject(string, GithubUserInforms.class);
+            return  githubUserInforms;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return  null;
+    }
+}
